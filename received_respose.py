5fa60496c8fd760c71857d6d2e18c873ede85249
Radu <122792139+GRaduRaduica@users.noreply.github.com>
2026-01-11 12:48:01 +0200

    Removing unwanted code bits
    Removing unwanted code bits

--- received_respose.py
+++ received_respose.py
@@ -2,10 +2,5 @@
 required only if what is received require translation
 """
 
-def recursive_factorial(n: int) -> int:
-    if n==0 or n==1:
-        return 1
-    return n*recursive_factorial(n-1)
-
-if __name__ == '__main__':
-    print(recursive_factorial(7))
+if __name__=='__main__':
+    pass
